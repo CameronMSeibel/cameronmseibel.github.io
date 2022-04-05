e53db19051903089fa7889359e437334e00c59a7
cmseibel <cam.m.seib@gmail.com>
2022-04-04 17:13:54 -0700

    Use animations with project cards.

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,41 +3,42 @@ import VisSensor from "react-visibility-sensor";
 import React, {useState} from "react";
 
 function Project({name, desc, image, links}){
+    const [active, setActive] = useState(false);
     return (
-        <VisSensor>
-        <Fade in={true}>
-        <Card sx={{maxWidth: "sm", margin: "2rem auto"}}>
-            <CardMedia
-                component="img"
-                image={image.src}
-                alt={image.alt}
-            />
-            <CardContent sx={{margin: "0 1rem"}}>
-                <Typography variant="h5" component="h3" gutterBottom>
-                    {name}
-                </Typography>
-                <Typography gutterBottom>
-                    {desc}
-                </Typography>
-                <Stack direction="row" justifyContent="center">
-                    <Button 
-                        target="_blank"
-                        href={links.deployed} 
-                        size="small"
-                    >
-                        Deployed
-                    </Button>
-                    <Button 
-                        target="_blank"
-                        href={links.github} 
-                        size="small"
-                    >
-                        GitHub
-                    </Button>
-                </Stack>
-            </CardContent>
-        </Card>
-        </Fade>
+        <VisSensor onChange={(isVisible) => isVisible ? setActive(true) : null}>
+            <Fade in={active}>
+            <Card sx={{maxWidth: "sm", margin: "2rem auto"}}>
+                <CardMedia
+                    component="img"
+                    image={image.src}
+                    alt={image.alt}
+                />
+                <CardContent sx={{margin: "0 1rem"}}>
+                    <Typography variant="h5" component="h3" gutterBottom>
+                        {name}
+                    </Typography>
+                    <Typography gutterBottom>
+                        {desc}
+                    </Typography>
+                    <Stack direction="row" justifyContent="center">
+                        <Button 
+                            target="_blank"
+                            href={links.deployed} 
+                            size="small"
+                        >
+                            Deployed
+                        </Button>
+                        <Button 
+                            target="_blank"
+                            href={links.github} 
+                            size="small"
+                        >
+                            GitHub
+                        </Button>
+                    </Stack>
+                </CardContent>
+            </Card>
+            </Fade>
         </VisSensor>
     );
 }
